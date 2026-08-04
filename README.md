728c3549b05342e844a1ea7f798501b70c2fbd4d
Stefano Maurogiovanni <s.maurogiovanni@fz-juelich.de>
2026-08-05 00:47:21 +0200

    ci(repo): initial commit
    Provide initial setup for the main branch of the repository. Only a minimal set of dependencies and the main development dependencies are installed and listed.

---
+++ README.md
@@ -0,0 +1,5 @@
+<!--
+SPDX-FileCopyrightText: 2026 Stefano Maurogiovanni <s.maurogiovanni@gmx.de>
+
+SPDX-License-Identifier: MIT
+-->
---
+++ src/multimodal_lucas/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2026 Stefano Maurogiovanni <s.maurogiovanni@gmx.de>
+#
+# SPDX-License-Identifier: MIT
+
+__version__ = "0.1.0a0"
